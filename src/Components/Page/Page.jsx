772d06c14ee7190c323c8deb7d4bf70625479f44
tinytinyone <kkk22kkk11kkk@mail.ru>
2022-11-11 12:17:54 +0300

    commit

--- src/Components/Page/Page.jsx
+++ src/Components/Page/Page.jsx
@@ -3,8 +3,9 @@ import './Page.css';
 
 const Page = () => {
   return (
-    <div>
-      <h2>Hello</h2>
+    <div className='wrapper-page'>
+      <h2>I Love You!</h2>
+      <img className='minion-hello' alt='img' src='https://i.pinimg.com/originals/2d/a5/cc/2da5cccdaa10e142846390f3851feb46.gif'/>
     </div>
   )
 }
